708c48125ad7005483be827da73dd23faf1e2398
Gianluigi Palmisano <gianlupalmi01@gmail.com>
2021-01-21 13:25:39 +0100

    Updated code
    Cambiate le domande in lingua italiana. Le domande vengono poste tramite text to speech in italiano ma le risposte vengono acquisite tramite tastiera.

--- my_code.py
+++ my_code.py
@@ -14,49 +14,62 @@ document.add_picture(
 )
 
 
-
 #name, phone number and email
-name = input('What is your name? ')
-speak('Hello '+ name + 'how are you today?')
+speak('Ciao, qual è il tuo nome?')
+firstname = input('Ciao, qual è il tuo nome? ')
+speak(firstname +', qual è il tuo cognome? ')
+lastname = input(firstname +', qual è il tuo cognome? ')
 
-speak('What is your phone number? ')
-phone_number = input()
-email = input('What is your email? ')
+name=firstname + ' ' + lastname
+
+speak('Qual''è il tuo numero di telefono?')
+phone_number = input('Qual''è il tuo numero di telefono? ')
+
+speak('Qual''è la tua mail?')
+email = input('Qual''è la tua mail? ')
 
 document.add_paragraph(
     name + ' | ' + phone_number + ' | ' + email
 )
 
 #about me
-document.add_heading('About me')
-about_me = input('Tell me about yourself ')
+document.add_heading('Chi sono')
+speak('Parlami di te')
+about_me = input('Parlami di te ')
 document.add_paragraph(about_me)
 
 #work experience
-document.add_heading('Work Experience')
+document.add_heading('Esperienze lavorative')
 p = document.add_paragraph()
 
-company = input('Enter company ')
-from_date = input('From Date')
-to_date = input('To Date')
+speak('In quale azienda hai lavorato?')
+company = input('Inserisci azienda ')
+speak('Da quando hai lavorato in '+company+'?')
+from_date = input('Dalla data ')
+speak('Fino a quando?')
+to_date = input('Alla data ')
 
 p.add_run(company + ' ').bold = True
 p.add_run(from_date + '-' + to_date + '\n').italic = True
 
+speak('Descrivi la tua esperienza')
 experience_details = input(
-    'Describe your experience at ' + company)
+    'Descrivi la tua esperienza in ' + company)
 p.add_run(experience_details)
 
 #skills
-document.add_heading('Skills')
-skill = input('Insert skill: ')
+document.add_heading('Competenze')
+speak('Quali competenze hai?')
+skill = input('Inserisci competenza ')
 p = document.add_paragraph(skill)
 p.style = 'List Bullet'
 
 while True:
-    q = input('Do you want to add another skill ? Yes/No')
-    if q.lower() == 'yes' or q.lower() == 'y':
-        skill = input('Insert skill: ')
+    speak('Vuoi inserire un''altra competenza?')
+    q = input('Vuoi inserire un''altra competenza? si/no ')
+    if q.lower() == 'si' or q.lower() == 's':
+        speak('Quali competenze hai?')
+        skill = input('Inserisci competenza ')
         p = document.add_paragraph(skill)
         p.style= 'List Bullet'
     else:
@@ -64,17 +77,22 @@ while True:
 
 #more experiences
 while True:
-    has_more_exp = input('Do you have more work experiences? ')
-    if has_more_exp.lower == 'yes':
-        company = input('Enter company ')
-        from_date = input('From Date')
-        to_date = input('To Date')
+    speak('Hai altre esperienze lavorative?')
+    has_more_exp = input('Hai altre esperienze lavorative? si/no ')
+    if has_more_exp.lower == 'si' or has_more_exp.lower() == 's':
+        speak('In quale azienda hai lavorato?')
+        company = input('Inserisci azienda ')
+        speak('Da quando hai lavorato in '+company+'?')
+        from_date = input('Dalla data ')
+        speak('Fino a quando?')
+        to_date = input('Alla data ')
 
         p.add_run(company + ' ').bold = True
         p.add_run(from_date + '-' + to_date + '\n').italic = True
 
+        speak('Descrivi la tua esperienza')
         experience_details = input(
-        'Describe your experience at ' + company)
+          'Descrivi la tua esperienza in ' + company)
         p.add_run(experience_details)
     else:
         break
