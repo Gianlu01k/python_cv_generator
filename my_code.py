1ebc419effa84be3791911518cbbb562d27ed390
Gianluigi Palmisano <gianlupalmi01@gmail.com>
2021-01-21 20:42:46 +0100

    Several updates
    La maggior parte delle risposte vengono effettuate tramite la voce. Lo SpeechRecognition permette di crearfe il cv rispondendo a domande vocali effettuate dal codice.

--- my_code.py
+++ my_code.py
@@ -1,24 +1,39 @@
 from docx import Document
 from docx.shared import Inches
 import pyttsx3
+import speech_recognition as sr
 
 def speak(text):
     pyttsx3.speak(text)
 
+#recognizer init
+recognizer_instance = sr.Recognizer() 
+
+def transcribe():
+    with sr.Microphone() as source:
+        recognizer_instance.adjust_for_ambient_noise(source)
+        print("...")
+        audio = recognizer_instance.listen(source)
+    try:
+        text = recognizer_instance.recognize_google(audio, language="it-IT")
+        return text
+    except Exception as e:
+        print(e)
+
 document = Document()
 
 #profile picture
-document.add_picture(
-    'profile_picture.jpg',
-    width=Inches(2.0)
-)
+#document.add_picture(
+#    'profile_picture.jpg',
+#    width=Inches(2.0)
+#)
 
 
 #name, phone number and email
 speak('Ciao, qual è il tuo nome?')
-firstname = input('Ciao, qual è il tuo nome? ')
+firstname = transcribe()
 speak(firstname +', qual è il tuo cognome? ')
-lastname = input(firstname +', qual è il tuo cognome? ')
+lastname = transcribe()
 
 name=firstname + ' ' + lastname
 
@@ -26,7 +41,7 @@ speak('Qual''è il tuo numero di telefono?')
 phone_number = input('Qual''è il tuo numero di telefono? ')
 
 speak('Qual''è la tua mail?')
-email = input('Qual''è la tua mail? ')
+email = transcribe()
 
 document.add_paragraph(
     name + ' | ' + phone_number + ' | ' + email
@@ -35,7 +50,7 @@ document.add_paragraph(
 #about me
 document.add_heading('Chi sono')
 speak('Parlami di te')
-about_me = input('Parlami di te ')
+about_me = transcribe()
 document.add_paragraph(about_me)
 
 #work experience
@@ -43,7 +58,7 @@ document.add_heading('Esperienze lavorative')
 p = document.add_paragraph()
 
 speak('In quale azienda hai lavorato?')
-company = input('Inserisci azienda ')
+company = transcribe()
 speak('Da quando hai lavorato in '+company+'?')
 from_date = input('Dalla data ')
 speak('Fino a quando?')
@@ -52,24 +67,23 @@ to_date = input('Alla data ')
 p.add_run(company + ' ').bold = True
 p.add_run(from_date + '-' + to_date + '\n').italic = True
 
-speak('Descrivi la tua esperienza')
-experience_details = input(
-    'Descrivi la tua esperienza in ' + company)
+speak('Descrivi la tua esperienza in ' + company)
+experience_details = transcribe()
 p.add_run(experience_details)
 
 #skills
 document.add_heading('Competenze')
 speak('Quali competenze hai?')
-skill = input('Inserisci competenza ')
+skill = transcribe()
 p = document.add_paragraph(skill)
 p.style = 'List Bullet'
 
 while True:
-    speak('Vuoi inserire un''altra competenza?')
-    q = input('Vuoi inserire un''altra competenza? si/no ')
-    if q.lower() == 'si' or q.lower() == 's':
+    speak('Vuoi inserire un''altra competenza? Rispondere SI o NO')
+    q = transcribe()
+    if q.lower() == 'si' :
         speak('Quali competenze hai?')
-        skill = input('Inserisci competenza ')
+        skill = transcribe()
         p = document.add_paragraph(skill)
         p.style= 'List Bullet'
     else:
@@ -77,11 +91,11 @@ while True:
 
 #more experiences
 while True:
-    speak('Hai altre esperienze lavorative?')
-    has_more_exp = input('Hai altre esperienze lavorative? si/no ')
-    if has_more_exp.lower == 'si' or has_more_exp.lower() == 's':
+    speak('Hai altre esperienze lavorative? Rispondi SI o NO')
+    has_more_exp = transcribe()
+    if has_more_exp.lower == 'si':
         speak('In quale azienda hai lavorato?')
-        company = input('Inserisci azienda ')
+        company = transcribe()
         speak('Da quando hai lavorato in '+company+'?')
         from_date = input('Dalla data ')
         speak('Fino a quando?')
@@ -90,9 +104,8 @@ while True:
         p.add_run(company + ' ').bold = True
         p.add_run(from_date + '-' + to_date + '\n').italic = True
 
-        speak('Descrivi la tua esperienza')
-        experience_details = input(
-          'Descrivi la tua esperienza in ' + company)
+        speak('Descrivi la tua esperienza in '+company)
+        experience_details = transcribe()
         p.add_run(experience_details)
     else:
         break
